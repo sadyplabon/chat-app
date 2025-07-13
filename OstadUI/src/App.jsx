3565065ef549377e12c885dcab23b34fad527eb4
Sady Al Mahedy <85930285+sadyplabon@users.noreply.github.com>
2025-07-13 19:31:06 +0600

    Update App.jsx IP address

--- OstadUI/src/App.jsx
+++ OstadUI/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
   // Fetch registered students
   const fetchStudents = async () => {
     try {
-      const response = await fetch("http://localhost:5050/getStudents");
+      const response = await fetch("http://16.170.254.33:5050/getStudents");
       const data = await response.json();
       setStudents(data);
     } catch (error) {
@@ -45,7 +45,7 @@ const handleSubmit = async (e) => {
 
   // Use toast.promise
   toast.promise(
-    fetch("http://localhost:5050/addStudent", {
+    fetch("http://16.170.254.33:5050/addStudent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
